e5a1e62ca6e62aaf8d71eaa7a29c7d9ecaf37970
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2024-05-14 09:28:23 -0400

    more tasteful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,24 @@
 import Image from "next/image"
 import tileImage from '/public/tile.png';
 import promImage from '/public/prom.jpg';
-import socrates from '/public/socrates.jpg';
+import lamb from '/public/mysticlamb.png';
 
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import React, { useState, useEffect } from 'react'
 
 export default function Home() {
   const [img, setImg] = useState(tileImage);
-
+  const [caption, setCaption] = useState("DALLE-2, 2022 AD");
+  const [lastY, setLastY] = useState(null);
+  
   const handleMouseEnter = () => {
-    setImg(promImage);
+    setImg(lamb);
+    setCaption("van Eyck, 1432 AD");
   };
 
   const handleMouseLeave = () => {
     setImg(tileImage);
+    setCaption("DALLE-2, 2022 AD");
   }
   
 
@@ -26,32 +30,35 @@ export default function Home() {
 
       <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <AspectRatio ratio={16 / 7.5}>
-          <Image src={img} alt="Image" className="rounded-md object-cover" width={500} height={100}/>
+          <Image src={img} alt="Image" className="rounded-md object-cover" width={800} height={100}/>
+          <h3 className="leading-7 text-right [&:not(:first-child)]:mt-1">{caption}</h3>
         </AspectRatio>
       </div>
 
-      <h2 className="scroll-m-20 text-xl font-semibold tracking-tight">Rishi Gurjar</h2>
-      <div >
-        <h4>SF Bay Area & Ithaca, NY</h4>
-        <h4>Email: x@y | x = rrg85, y = cornell.edu</h4>
-        <h4>Gardening and crafting</h4>
-      </div>
-      <br />
+      <div className="mt-4">
+        <h2 className="scroll-m-20 text-xl font-semibold tracking-tight">Rishi Gurjar</h2>
+        <div >
+          <h4>SF Bay Area & Ithaca, NY</h4>
+          <h4>Email: x@y | x = rrg85, y = cornell.edu</h4>
+          <h4>Gardening and crafting</h4>
+        </div>
+        <br />
 
-      <h2 className="leading-7 [&:not(:first-child)]:mt-1">Socials</h2>
-      <div>
-        <a className="hover:text-blue-500" href="https://www.linkedin.com/in/rishigurjar/" target="_blank" rel="noopener noreferrer"><h4>Linkedin</h4></a>
-        <a className="hover:text-blue-500" href="https://x.com/rishi__gurjar" target="_blank" rel="noopener noreferrer"><h4>Twitter</h4></a>
-        <a className="hover:text-blue-500" href="https://github.com/rishi-gurjar" target="_blank" rel="noopener noreferrer"><h4>GitHub</h4></a>
-        <a className="hover:text-blue-500" href="https://unsplash.com/@rishigurjar/" target="_blank" rel="noopener noreferrer"><h4>Unsplash</h4></a>
-      </div>
-      <br />
+        <h2 className="leading-7 [&:not(:first-child)]:mt-1">Socials</h2>
+        <div>
+          <a className="hover:text-blue-500" href="https://www.linkedin.com/in/rishigurjar/" target="_blank" rel="noopener noreferrer"><h4>Linkedin</h4></a>
+          <a className="hover:text-blue-500" href="https://x.com/rishi__gurjar" target="_blank" rel="noopener noreferrer"><h4>Twitter</h4></a>
+          <a className="hover:text-blue-500" href="https://github.com/rishi-gurjar" target="_blank" rel="noopener noreferrer"><h4>GitHub</h4></a>
+          <a className="hover:text-blue-500" href="https://unsplash.com/@rishigurjar/" target="_blank" rel="noopener noreferrer"><h4>Unsplash</h4></a>
+        </div>
+        <br />
 
-      <h2 className="leading-7 [&:not(:first-child)]:mt-1">Recent projects</h2>
-      <div>
-        <a className="hover:text-green-500" href="https://coldcraft.ai" target="_blank" rel="noopener noreferrer"><h4>ColdCraft</h4></a>
-        <a className="hover:text-green-500" href="https://x.com/cornellarmada" target="_blank" rel="noopener noreferrer"><h4>Armada</h4></a>
-        <a className="hover:text-green-500" href="https://beyondterra.org" target="_blank" rel="noopener noreferrer"><h4>Beyond Terra</h4></a>
+        <h2 className="leading-7 [&:not(:first-child)]:mt-1">Recent projects</h2>
+        <div>
+          <a className="hover:text-green-500" href="https://coldcraft.ai" target="_blank" rel="noopener noreferrer"><h4>ColdCraft</h4></a>
+          <a className="hover:text-green-500" href="https://x.com/cornellarmada" target="_blank" rel="noopener noreferrer"><h4>Armada</h4></a>
+          <a className="hover:text-green-500" href="https://beyondterra.org" target="_blank" rel="noopener noreferrer"><h4>Beyond Terra</h4></a>
+        </div>
       </div>
 
     </main>
