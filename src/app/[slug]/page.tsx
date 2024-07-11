9204b352d2fb258cc9db72168c216c465672653b
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2024-07-11 11:26:24 -0700

    slugs

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,14 +4,20 @@ import ReactMarkdown from 'react-markdown'
 
 // This function generates the static params at build time
 export async function generateStaticParams() {
-  const res = await fetch('https://api.github.com/repos/rishi-gurjar/webpage/contents/writings')
-  const files = await res.json()
-  
-  return files
-    .filter((file: any) => file.name.endsWith('.md'))
-    .map((file: any) => ({
-      slug: file.name.replace(/\.md$/, '')
-    }))
+  // const res = await fetch('https://api.github.com/repos/rishi-gurjar/webpage/contents/writings')
+  // const files = await res.json()
+  // const files_filtered = files
+  // .filter((file: any) => file.name.endsWith('.md'))
+  // .map((file: any) => ({
+  //   slug: file.name.replace(/\.md$/, '')
+  // }))
+  // console.log(files_filtered)
+
+  return [ 
+  { slug: 'habeas' },
+  { slug: 'gradient'},
+  { slug: 'test'}
+   ]
 }
 
 // This function generates metadata for the page
