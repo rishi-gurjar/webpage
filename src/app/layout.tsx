7fd0fc0b41158073044a30940380c6ddc6765121
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2024-10-17 21:01:51 -0400

    LOCADAPT!!! ٱلْمُصَوِّرُٱلْمُصَوِّرُٱلْمُصَوِّرُ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link
+          rel="stylesheet"
+          href="https://cdn.locadapt.com/locadapt.min.css"
+      />
+      <script
+          src="https://cdn.locadapt.com/locadapt.min.js"
+          data-project-id="85466916-e58a-461c-8ca7-5bbb38af4d92"
+          data-ssr-defer
+      />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
