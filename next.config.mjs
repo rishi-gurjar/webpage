d6e84c1d922da47417eedae98d32f4843618ecc0
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2024-12-18 22:44:37 -0500

    github link :( embarrassing i cant even locally host imgs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,41 @@
 const nextConfig = {
   experimental: {
     serverActions: true,
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'user-attachments.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github-production-user-asset-6210df.s3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      type: 'asset/resource'
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
