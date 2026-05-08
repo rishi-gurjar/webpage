1a93e04f99255ae646cfdf7c1b76101a0aab5a89
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2026-05-08 13:33:08 -0700

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://capital-seriously-ghoul.ngrok-free.app',
+  },
 };
 
 export default nextConfig;
